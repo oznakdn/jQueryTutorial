7ff2161eaa44b265913d58546f510b4249e1e834
Ozan Akaydin <ozanakaydin@gmail.com>
2022-06-02 16:55:57 +0300

    ok

--- Beginner/jQueryUI/UIExamples/1-jqueryUI.js
+++ Beginner/jQueryUI/UIExamples/1-jqueryUI.js
@@ -1,5 +1,8 @@
-$(document).ready(function(){
-
 $("#date").datepicker();
+
+$("#tool").tooltip({
+    track:true,
+    show:{effect:"slideDown",duration:2000,delay:250},
+    hide:{effect:"slideUp",duration:2000,delay:250}
 
 });
